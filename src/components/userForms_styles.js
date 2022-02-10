f1a286f1c234411103225fb7ea7bcc9807a7ed1c
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-09 22:05:57 -0300

    test: adjusting 'Container' padding

--- src/components/userForms_styles.js
+++ src/components/userForms_styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  padding: 20px;
+  padding: 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
