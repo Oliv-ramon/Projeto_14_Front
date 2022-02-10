383bff02621e49aa5db21c8456d9563308cb36f3
ramon oliveira <ramon30012000@gmail.com>
2022-02-10 09:51:13 -0300

    preparing main branch to receive the sign-up branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./contexts/AuthContext";
-import { SignIn } from "./pages";
+import { SignIn, SignUp } from "./pages";
 
 function App()  {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={"<MainPage/>"}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
           <Route path="/sign-in" element={<SignIn/>}/>
         </Routes>
       </BrowserRouter>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import SignIn from "./SignIn";
+import SignUP from "./SignUp";
 
 export {
+  SignUP,
   SignIn,
 }
