aee5d9f118aa27d8ab17445e1a11aedc90117edf
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-14 15:32:23 -0300

    patch: displaying currency correctly

--- src/pages/MainPage/ProductCard.js
+++ src/pages/MainPage/ProductCard.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { ItemCard } from "../../components";
 
 export default function ProductCard({children}) {
+
+  const displayPrice = ((children.price)/100).toFixed(2).toLocaleString('pt-br', {style: 'currency', currency:'BRL'});
+  
   return (
     <ItemCard>
       <img src={children.image} alt='product thumbnail'/>
       <h2>{children.description}</h2>
-      <h3>R$ {children.price}</h3>
+      <h3>R$ {displayPrice}</h3>
     </ItemCard>
   ); 
 };
